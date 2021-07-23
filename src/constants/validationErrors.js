7fb570739fae22e4a1ec876064324c2d2481fec5
Evgeny Mandybura <evgeny.mandybura.cr@gmail.com>
2021-07-23 11:14:28 +0300

    [WIP] WEBI-255 Fix log work time

--- src/constants/validationErrors.js
+++ src/constants/validationErrors.js
@@ -62,6 +62,9 @@ export const IS_INCORRECT_LOGGED_TIME = `Data must be in "3w 5d 10h 30m" format`
 
 //reportsFilter
 export const FINISH_DATE_IS_INCORRECT =
-    "Finish Date must be greater than Start Date";
+  "Finish Date must be greater than Start Date";
 export const DATE_RANGE_IS_INCORRECT =
-    "The date range must be less than 31 days.";
+  "The date range must be less than 31 days.";
+
+export const WRONG_USER =
+  "It is impossible to log the time, the task is attached to another user";
